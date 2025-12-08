bf1fea230aa2e9fcd3c55c6edcf46a273805a56c
jisulee <jisulee@example.com>
2025-12-08 15:40:44 +0900

    fix: postcss.config.js를 ES 모듈 형식으로 수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
   plugins: {
-    tailwindcss: require('tailwindcss'),
-    autoprefixer: require('autoprefixer'),
+    tailwindcss,
+    autoprefixer,
   },
 };
 
